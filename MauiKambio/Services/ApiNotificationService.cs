ecb4b3f68ca0f80e1d14b18d8bf74fe9ca3bf8e1
Francisco Carmona <flcarmonac@yahoo.com>
2024-11-13 14:20:28 -0300

    Update Notification Page

--- MauiKambio/Services/ApiNotificationService.cs
+++ MauiKambio/Services/ApiNotificationService.cs
@@ -30,6 +30,9 @@ namespace MauiKambio.Services
                 new NotificationDTO() { Id = 3, User_Id = 1003, User_Name = "Nombre 3", User_Img = "user.jpg", Offer_Description = "Tengo una Tetera para cambiar" },
                 new NotificationDTO() { Id = 4, User_Id = 1004, User_Name = "Nombre 4", User_Img = "user.jpg", Offer_Description = "Tengo una Casa para cambiar" },
                 new NotificationDTO() { Id = 5, User_Id = 1005, User_Name = "Nombre 5", User_Img = "user.jpg", Offer_Description = "Tengo una Guitarra para cambiar" },
+                new NotificationDTO() { Id = 6, User_Id = 1006, User_Name = "Nombre 6", User_Img = "user.jpg", Offer_Description = "Tengo un León para cambiar" },
+                new NotificationDTO() { Id = 7, User_Id = 1007, User_Name = "Nombre 7", User_Img = "user.jpg", Offer_Description = "Tengo una Esposa para cambiar" },
+                new NotificationDTO() { Id = 8, User_Id = 1008, User_Name = "Nombre 8", User_Img = "user.jpg", Offer_Description = "Tengo 20 Japonesas para cambiar" },
             };
         }
     }
